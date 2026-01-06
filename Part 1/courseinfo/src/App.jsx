f535d8d9495356118802292500a45d1ab1fce233
Mauro Alegre <maurito2590@gmail.com>
2026-01-06 03:54:40 -0300

    make Header and Content component

--- Part 1/courseinfo/src/App.jsx
+++ Part 1/courseinfo/src/App.jsx
@@ -1,3 +1,41 @@
+// Make header
+const Header = (props) => {
+
+  return (
+    <>
+    <h1>
+      {props.course}
+    </h1>
+    </>
+  )
+}
+
+// Make *Part* component to use on content
+const Part = (prop) => {
+
+  return (
+    <p>
+      {prop.part}, {prop.exercise}
+    </p>
+  )
+}
+
+
+// Make *Content* componen
+const Content = (props) => {
+  
+
+  return (
+    <div>
+      < Part part = {props.part1} exercise = {props.exercises1} />
+      < Part part = {props.part2} exercise = {props.exercises2} />
+      < Part part = {props.part3} exercise = {props.exercises3} />
+    </div>
+  )
+}
+
+
+
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,16 +47,14 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      < Header 
+      course = {course} 
+      />
+      <Content 
+      part1 = {part1} exercises1 = {exercises1}
+      part2 = {part2} exercises2 = {exercises2}
+      part3 = {part3} exercises3 = {exercises3}
+      />
       <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
