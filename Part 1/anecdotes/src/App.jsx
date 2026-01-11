ec5847780d3e0bbad07356e255a575323e11a136
Mauro Alegre <maurito2590@gmail.com>
2026-01-10 23:34:20 -0300

    finish part 1

--- Part 1/anecdotes/src/App.jsx
+++ Part 1/anecdotes/src/App.jsx
@@ -1,10 +1,65 @@
 import { useState } from 'react'
 
 
+const Buttons = ({next, vote}) => {
+
+  const text = ['Vote', 'Next anecdote'];
+
+  return (
+   
+    <div>
+      <button onClick={vote}>
+        {text[0]}
+      </button>
+      <button onClick={next}>
+        {text[1]}
+      </button>
+    </div>
+  )
+};
 
 
+const Most = ({anecdote, count}) => {
 
+  let index = 0;
+  let max = count[0];
 
+  for (let i = 1; i < count.length; i++) {
+    if (count[i] > max) {
+      max = count[i];
+      index = i;
+    }
+  }
+
+  console.log(max)
+  console.log(anecdote[index])
+
+  if (max === 0) {
+    
+    return ( 
+      <p>
+        <strong>
+          No votes yet
+        </strong>
+      </p>     
+    )
+  }
+
+  return (
+    <>
+    <h2>
+      Anecdote with most votes
+    </h2>
+    <p>
+      {anecdote[index]}
+    </p>
+    <p>
+      Has {max} votes
+    </p>
+    </>
+  )
+
+};
 
 
 
@@ -21,11 +76,55 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
 
-  const [selected, setSelected] = useState(0)
+  
+  const [selected, setSelected] = useState(0);
+  const [current, setCurrent] = useState(0)
+  
+  
+  const arr = new Array(anecdotes.length).fill(0);
+  const [voted, setVoted] = useState(arr)
+  
+  const handleClickNext = () => {
+
+    const randomNum = () => {
+
+      let random = 0
+
+      do {
+
+        random = Math.floor(Math.random() * anecdotes.length)
+      } while (random === current);
+
+      setCurrent(random);
+      return random;
+
+    } 
+  
+    setSelected(randomNum);
+  }
+
+
+  const handleClickVotes = () => {
+
+    const votes = [...voted]
+    votes[selected] += 1
+    setVoted(votes)
+  }
 
   return (
+
     <div>
-      {anecdotes[selected]}
+      <h2>
+        Anecdote of the day
+      </h2>
+      <p>
+        {anecdotes[selected]}
+      </p>
+      <p>
+        Has {voted[anecdotes.indexOf(anecdotes[selected])]} votes
+      </p>
+      < Buttons next={handleClickNext} vote={handleClickVotes} />
+      < Most anecdote={anecdotes} count={voted} />
     </div>
   )
 }
