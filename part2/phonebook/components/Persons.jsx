9121b0818c635f21a77b75cb1e90566a8efaac68
Mauro Alegre <maurito2590@gmail.com>
2026-01-15 04:31:25 -0300

    add db local

--- part2/phonebook/components/Persons.jsx
+++ part2/phonebook/components/Persons.jsx
@@ -3,13 +3,13 @@ import Name from "./Name";
 // Component
 const Persons = ({list, filter}) => {
 
-     const filtered = list.filter((item) => item.name.toLowerCase().includes(filter.toLowerCase()));
-
+/*      const filtered = list.filter((item) => item.name.toLowerCase().includes(filter.toLowerCase()));
+ */
   return (
     <div>
-      {filtered.map(item => (
+{/*       {filtered.map(item => (
       < Name key={item.id} name={item.name} number={item.number} />
-    ))}
+    ))} */}
     </div>
   );
 };
